cb89a34ce34d2ab2b833b00619fcde26eebaf324
amara <aniamara70@gmail.com>
2022-07-18 11:53:50 +0100

    removed csrf in my index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -27,7 +27,6 @@
                             @endif 
                             <form method="POST" action="{{route('create.user')}}">
                                 @csrf
-                                @method("PUT")
                                 <div class="form-group">
                                     <strong><label for="name">Name:</label></strong>
                                     <input type="text" name="name" class="form-control" placeholder="What is your fullname">
